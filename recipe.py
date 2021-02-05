6eff34b4e4377e16bf9a5245fd95a9c28b50a8d1
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-02-06 00:20:41 +0100

    add js

--- recipe.py
+++ recipe.py
@@ -19,8 +19,10 @@ with open("ecolab-alimentation/data/out/Agribalyse.json", "r") as f:
 
 
 class Recipe():
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.ingredients = {}
+        self.content = {}
         with open("ecolab-alimentation/data/out/Agribalyse.json", "r") as f:
             self.agribalyse = json.loads(f.read())
         with open("table Ciqual 2020_ENG_2020 07 07.json", "r") as f:
@@ -38,13 +40,19 @@ class Recipe():
         self.CO2e = 0
         self.kcal = 0
         for name in self.ingredients.keys():
-            code, ef, kcal100g = self.query(name,"Changement climatique")
-            self.ingredients[name]["CO2e"] =  ef * (self.ingredients[name]["quantity"]/1000)
-            self.ingredients[name]["kcal"] =  int(kcal100g * (self.ingredients[name]["quantity"]/100))
-            self.ingredients[name]["code"] = code
+            self.content[name] = {}
+            entries = self.add_values(name)
+            for key, value in entries.items():
+                self.content[name][key] =  value
+            self.content[name]["CO2e"] =  self.content[name]["ef"] * (self.ingredients[name]["quantity"]/1000)
+            try:
+                self.content[name]["kcal"] =  int(self.content[name]["Energy, Regulation EU No 1169/2011 (kcal/100g)"]) * \
+                                                    (self.ingredients[name]["quantity"]/100)
+            except:
+                self.content[name]["kcal"] = 0
             self.weight += self.ingredients[name]["quantity"]
-            self.CO2e += self.ingredients[name]["CO2e"]
-            self.kcal += self.ingredients[name]["kcal"]
+            self.CO2e += self.content[name]["CO2e"]
+            self.kcal += self.content[name]["kcal"]
         self.CO2e = round(self.CO2e,2)
     
     def query(self,name,parameter):
@@ -57,9 +65,19 @@ class Recipe():
                 except:
                     kcal100g = 0
                 return code, ef, kcal100g
+            
+    def add_values(self,name):
+        entries = {}
+        for entry in self.agribalyse:
+            if entry["LCI_name"] == name:
+                entries["code"] = entry["ciqual_code"]
+                entries["ef"] = entry["impact_environnemental"]["Changement climatique"]["synthese"]
+                for key, value in self.ciqual[entries["code"]].items():
+                    entries[key] = value
+                return entries
 
 
-my_recipe = Recipe()
+my_recipe = Recipe("Pasta broccoli e aggiughe")
 # my_recipe.addIngredient("Dried pasta, wholemeal, raw", 100)
 # my_recipe.addIngredient("Olive oil, extra virgin", 2)
 # my_recipe.addIngredient("Anchovy, in salt (semi-preserved)", 15)
@@ -69,12 +87,13 @@ my_recipe.addIngredients([("Dried pasta, wholemeal, raw", 400),
                           ("Anchovy, in salt (semi-preserved)", 50),
                           ("Romanesco cauliflower or romanesco broccoli, raw", 1000)])
 my_recipe.cook()
+print(my_recipe.name)
 print(my_recipe.ingredients)
 print("Weight",my_recipe.weight,"g")
 print("Footprint",my_recipe.CO2e,"kg CO2e")
 print("Calories",my_recipe.kcal,"kcal")
 
-my_recipe2 = Recipe()
+my_recipe2 = Recipe("Tagliatelle al ragù")
 my_recipe2.addIngredients([("Dried egg pasta, raw", 400),
                           ("Olive oil, extra virgin", 2),
                           ("Beef, minced steak, 20% fat, cooked", 300),
@@ -83,6 +102,7 @@ my_recipe2.addIngredients([("Dried egg pasta, raw", 400),
                           ("Onion, raw", 50),
                           ("Tomato coulis, canned (tomato puree semi-reduced 11%)", 300)])
 my_recipe2.cook()
+print(my_recipe2.name)
 print(my_recipe2.ingredients)
 print("Weight",my_recipe2.weight,"g")
 print("Footprint",my_recipe2.CO2e,"kg CO2e")
