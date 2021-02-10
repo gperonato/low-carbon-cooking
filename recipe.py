547001a9a4764a16dc6d88345c1a1f71585e25fb
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-02-10 21:48:22 +0100

    cooking model

--- recipe.py
+++ recipe.py
@@ -11,11 +11,15 @@ import csv
 
 
 class Recipe():
-    def __init__(self, name):
+    def __init__(self, name, country="France"):
         self.name = name
         self.ingredients = {}
         self.content = {}
+        self.cooking_steps = {}
         self.db = []
+        self.country = country
+
+        self.energy_ef = {"France":{"electricity": 0.0498, "gas": 0.244}}
         with open('data/join.csv') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
@@ -31,7 +35,19 @@ class Recipe():
         for ingredient in ingredients:
             self.addIngredient(ingredient[0],ingredient[1])
 
+    def addCookingStep(self,name,duration,energy_source,power):
+        self.cooking_steps[name] = {"duration": duration,
+                                    "energy_source": energy_source,
+                                    "power": power}
+
     def cook(self):
+        for name in self.cooking_steps.keys():
+            energy = self.cooking_steps[name]["duration"]/60. * self.cooking_steps[name]["power"] / 1000.
+            self.cooking_steps[name]["CO2e"] = energy * self.energy_ef[self.country][self.cooking_steps[name]["energy_source"]]
+            self.CO2e +=  self.cooking_steps[name]["CO2e"] 
+        # self.CO2e = round(self.CO2e,2)
+
+    def mise_en_place(self):
         self.weight = 0
         self.CO2e = 0
         self.kcal = 0
@@ -49,7 +65,6 @@ class Recipe():
             self.weight += self.ingredients[name]["quantity"]
             self.CO2e += self.content[name]["CO2e"]
             self.kcal += self.content[name]["kcal"]
-        self.CO2e = round(self.CO2e,2)
     
     def add_values(self,name):
         entries = {}
@@ -72,13 +87,16 @@ my_recipe.addIngredients([("Dried pasta, wholemeal, raw", 400),
                           ("Olive oil, extra virgin", 2),
                           ("Anchovy, in salt (semi-preserved)", 50),
                           ("Romanesco cauliflower or romanesco broccoli, raw", 1000)])
-my_recipe.cook()
+my_recipe.mise_en_place()
 print(my_recipe.name)
 print(my_recipe.ingredients)
 print("Weight",my_recipe.weight,"g")
 print("Footprint",my_recipe.CO2e,"kg CO2e")
 print("Calories",my_recipe.kcal,"kcal")
-
+my_recipe.addCookingStep("cooking",15,"electricity", 2500)
+print(my_recipe.cooking_steps)
+my_recipe.cook()
+print("Footprint",my_recipe.CO2e,"kg CO2e")
 my_recipe2 = Recipe("Tagliatelle al ragù")
 my_recipe2.addIngredients([("Dried egg pasta, raw", 400),
                           ("Olive oil, extra virgin", 2),
@@ -87,7 +105,7 @@ my_recipe2.addIngredients([("Dried egg pasta, raw", 400),
                           ("Celery stalk, raw", 50),
                           ("Onion, raw", 50),
                           ("Tomato coulis, canned (tomato puree semi-reduced 11%)", 300)])
-my_recipe2.cook()
+my_recipe2.mise_en_place()
 print(my_recipe2.name)
 print(my_recipe2.ingredients)
 print("Weight",my_recipe2.weight,"g")
