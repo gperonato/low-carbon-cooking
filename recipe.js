a85262205376c1a9e0d62500ae122d6d7bc76478
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-02-18 23:53:27 +0100

    form layout

--- recipe.js
+++ recipe.js
@@ -1,20 +1,3 @@
-function loadJSON(path, success, error) {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success)
-					success(JSON.parse(xhr.responseText));
-			} else {
-				if (error)
-					error(xhr);
-			}
-		}
-	};
-	xhr.open("GET", path, true);
-	xhr.send();
-}
-
 function getNum(val) {
 	val = +parseFloat(val) || 0
 	return val;
@@ -199,7 +182,6 @@ function formsubmit() {
 }
 
 
- 
-run();
+
 
 
